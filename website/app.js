1013de2b3560aec4de3ec42db0f301cdd9614fa1
Robert Raney <rwraney3@gmail.com>
2021-03-27 15:23:34 -0400

    writing js

--- website/app.js
+++ website/app.js
@@ -1,4 +1,26 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = '&appid=0a1412ebc534bf33dafd0211321ab8cc'
+let country = ',us';
+const newZip = document.getElementById('zip').value;
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(){
+    const newZip =  document.getElementById('zip').value;
+    getZip(baseURL, newZip, country, apiKey)
+}
+const getZip = async (baseURL, zip, country, key)=> {
+    const res = await fetch(baseURL+zip+country+key)
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+  } catch(error) {
+      console.log("error", error);
+    // appropriately handle the error
+  }
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
